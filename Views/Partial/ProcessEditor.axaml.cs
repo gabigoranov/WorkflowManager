eb2955a364b5d8ec32d1d2230e49387a12f0f220
gabigoranov <gabigoranov@abv.bg>
2026-02-09 22:17:45 +0200

    Split up workflow editor into partial views

---
+++ Views/Partial/ProcessEditor.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace WorkflowManager.Views.Partial;
+
+public partial class ProcessEditor : UserControl
+{
+    public ProcessEditor()
+    {
+        InitializeComponent();
+    }
+}
